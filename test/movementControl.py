4da16a700f69cc01aa0505e206de6646f5bebbd6
Smileything <150485186+Smileything@users.noreply.github.com>
2025-11-06 14:13:15 +0100

    diffrent movement control

--- test/movementControl.py
+++ test/movementControl.py
@@ -1,15 +1,28 @@
-from Rosmaster_Lib import Rosmaster
-import time
-
 class MovementControl:
     def __init__(self):
         self.g_bot = Rosmaster()
+        self.speedX = 0
+        self.speedY = 0
+        self.speedZ = 0
 
     def stop(self):
         self.g_bot.set_car_motion(0,0,0)
     
-    def forwards(self, speed):
-        self.g_bot.set_car_motion(speed,0,0)
+    def drive(self):
+        self.g_bot.set_car_motion(self.speedX, self.speedY, self.speedZ)
+    
+    def setSpeedX(self, speed):
+        self.setSpeedX = speed
+
+    def setSpeedY(self, speed):
+        self.setSpeedY = speed
+
+    def setSpeedZ(self, speed):
+        self.setSpeedZ = speed
+
+    def forwards(self):
+        self.setSpeedX(0.1)
+        self.drive
 
 #passiert wenn dieses script aufgerufen wird
 if __name__ == "__main__":
