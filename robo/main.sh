68c49cb4ff55c1bd8e12a7c5cf8021f7a597f9fa
Franka <franka.pawlak@siemens.com>
2025-11-24 15:49:03 +0100

    test

--- robo/main.sh
+++ robo/main.sh
@@ -1,9 +1,9 @@
-#python cameraHändler.py
+python cameraHändler.py
 #python audioHändler.py
 #python commandHändler.py
 
-python sendAudio.py &
-python sendLidar.py &
+#python sendAudio.py &
+#python sendLidar.py &
 python getCommands.py &
 
 wait
