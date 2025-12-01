e75c689aba4bcada51c03d93ed09b2b77e92240a
Franka <franka.pawlak@siemens.com>
2025-12-01 09:05:24 +0100

    remove debug statements

--- robo/movementControl.py
+++ robo/movementControl.py
@@ -12,54 +12,44 @@ class MovementControl:
     def fullstop(self):
         self.speedx = 0
         self.speedy = 0
-        self.speedz = 0
-        print(f"command executed: fullstop x: {self.speedx}, y: {self.speedy}, z: {self.speedz}")
+        self.speedz = 0 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def stopRotate(self):
-        self.speedz = 0
-        print(f"command executed: stopRotate z: {self.speedz}")
+        self.speedz = 0 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
         
 
     def stopLeftRight(self):
-        self.speedy = 0
-        print(f"command executed: stopLeftright y: {self.speedy}")
+        self.speedy = 0 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def stopForwardsBackwards(self):
         self.speedx = 0
-        print(f"command executed: stopForwardsbackwards x: {self.speedx}")
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def forwards(self):
-        self.speedx = self.speed
-        print(f"command executed: forwards x: {self.speedx}")
+        self.speedx = self.speed 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def backwards(self):
-        self.speedx = -self.speed
-        print(f"command executed: backwards x: {self.speedx}")
+        self.speedx = -self.speed 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def right(self):
-        self.speedy = -self.speed
-        print(f"command executed: right y: {self.speedy}")
+        self.speedy = -self.speed 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def left(self):
-        self.speedy = self.speed
-        print(f"command executed: left y: {self.speedy}")
+        self.speedy = self.speed 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def turnRight(self):
-        self.speedz = -1
-        print(f"command executed:turnRight  z: {self.speedz}")
+        self.speedz = -1 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
 
     def turnLeft(self):
-        self.speedz = 1
-        print(f"command executed: turnLeft z: {self.speedz}")
+        self.speedz = 1 
         self.g_bot.set_car_motion(self.speedx,self.speedy,self.speedz)
     
     def setSpeed(self, speed):
