bd0ecc8c639df2f3967363a21dc0f3a6dc612364
Simon <simon.kasper@siemens.com>
2025-12-01 13:46:46 +0100

    Added minimum Error Händling for Connection Threads

--- robo/sendLidar.py
+++ robo/sendLidar.py
@@ -7,7 +7,14 @@ IP = 'localhost'
 if __name__ == "__main__":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((IP, PORT))
-    while True:
-        client.send("Lidar Data".encode())
-        time.sleep(10)
+    try:
+        while True:
+            client.send("Lidar Data".encode())
+            time.sleep(10)
+    except Exception as e:
+        print(f"Error: {e}")
+    except KeyboardInterrupt:
+        print("Programm closed")
+    finally:
+        client.close()
 
