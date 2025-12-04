6641a894336efa293f67057b1f313ac4a7c9e2e7
Simon <simon.kasper@siemens.com>
2025-12-04 11:22:54 +0100

    Changed import Paths again...

--- src/server/app/ui.py
+++ src/server/app/ui.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
-import src.server.send_commands.sendcommands
+import server.send_commands.sendcommands as sendcommands
 
 index = ROOT / "server" / "templates" / "index.html"
 
@@ -18,33 +18,33 @@ def index():
 @app.route('/button-click', methods=['POST'])
 def button_click():
     data = request.get_json()
-    src.server.send_commands.sendcommands.ButtonClicked(f"{data['id']}")
+    sendcommands.ButtonClicked(f"{data['id']}")
     return '', 204
 @app.route('/button-click-inside', methods=['POST'])
 def button_click_inside():
     data = request.get_json()
-    src.server.send_commands.sendcommands.ButtonClickedInside(f"{data['id']}")
+    sendcommands.ButtonClickedInside(f"{data['id']}")
     return '', 204
 @app.route('/button-press', methods=['POST'])
 def button_press():
     data = request.get_json()
-    src.server.send_commands.sendcommands.ButtonPress(f"{data['id']}")
+    sendcommands.ButtonPress(f"{data['id']}")
     return '', 204
 @app.route('/button-release', methods=['POST'])
 def button_release():
     data = request.get_json()
-    src.server.send_commands.sendcommands.ButtonRelease(f"{data['id']}")
+    sendcommands.ButtonRelease(f"{data['id']}")
     return '', 204
 
 @app.route('/key-down', methods=['POST'])
 def key_down():
     data = request.get_json()
-    src.server.send_commands.sendcommands.ButtonPress(f"{data['key']}")
+    sendcommands.ButtonPress(f"{data['key']}")
     return '', 204
 @app.route('/key-up', methods=['POST'])
 def key_up():
     data = request.get_json()
-    src.server.send_commands.sendcommands.ButtonRelease(f"{data['key']}")
+    sendcommands.ButtonRelease(f"{data['key']}")
     return '', 204
 
 def start_ui():
