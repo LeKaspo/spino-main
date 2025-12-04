89ee14de18a6b5f489825f7ade4d200c859b4566
DinosLikePizza <kai.huebner@siemens.com>
2025-12-04 09:52:58 +0100

    init.py

--- src/server/app/ui.py
+++ src/server/app/ui.py
@@ -1,7 +1,13 @@
 #starte den Flaskserver übern den das web UI läuft
+import sys
+from pathlib import Path
 from flask import Flask, render_template, request
-import server.sendcommands
+import src.server.send_commands.sendcommands
 
+ROOT = Path(__file__).resolve().parents[2]
+sys.path.append(str(ROOT))
+
+camera_script = ROOT / "server" / "templates" / "index.html"
 
 app = Flask(__name__)
 @app.route('/')
@@ -12,33 +18,33 @@ def index():
 @app.route('/button-click', methods=['POST'])
 def button_click():
     data = request.get_json()
-    server.sendcommands.ButtonClicked(f"{data['id']}")
+    src.server.send_commands.sendcommands.ButtonClicked(f"{data['id']}")
     return '', 204
 @app.route('/button-click-inside', methods=['POST'])
 def button_click_inside():
     data = request.get_json()
-    server.sendcommands.ButtonClickedInside(f"{data['id']}")
+    src.server.send_commands.sendcommands.ButtonClickedInside(f"{data['id']}")
     return '', 204
 @app.route('/button-press', methods=['POST'])
 def button_press():
     data = request.get_json()
-    server.sendcommands.ButtonPress(f"{data['id']}")
+    src.server.send_commands.sendcommands.ButtonPress(f"{data['id']}")
     return '', 204
 @app.route('/button-release', methods=['POST'])
 def button_release():
     data = request.get_json()
-    server.sendcommands.ButtonRelease(f"{data['id']}")
+    src.server.send_commands.sendcommands.ButtonRelease(f"{data['id']}")
     return '', 204
 
 @app.route('/key-down', methods=['POST'])
 def key_down():
     data = request.get_json()
-    server.sendcommands.ButtonPress(f"{data['key']}")
+    src.server.send_commands.sendcommands.ButtonPress(f"{data['key']}")
     return '', 204
 @app.route('/key-up', methods=['POST'])
 def key_up():
     data = request.get_json()
-    server.sendcommands.ButtonRelease(f"{data['key']}")
+    src.server.send_commands.sendcommands.ButtonRelease(f"{data['key']}")
     return '', 204
 
 if __name__ == '__main__':
