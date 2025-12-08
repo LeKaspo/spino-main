660aaa385d4c01d7c2aab6afc95b5305f448ab1f
DinosLikePizza <kai.huebner@siemens.com>
2025-12-08 13:21:39 +0100

    changes for gesture

--- src/server/app/main.py
+++ src/server/app/main.py
@@ -1,14 +1,19 @@
 import sys
 from pathlib import Path  
+import threading
 
 ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
 from server.app.connection import connectionHändler
 import server.app.ui as ui
+import server.gesture.gesture as gesture
 
 connectionHändler.getInstance()
 #start robo
 ui.start_ui()
 
 # start threads
+
+t = threading.Thread(target=gesture.capture_loop, daemon=True)
+t.start()
--- src/server/app/ui.py
+++ src/server/app/ui.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template, request, Response
 ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
-import server.send_commands.sendcommands as sendcommands
+import server.send_commands.processcommands as processcommands
+import server.gesture.gesture as gesture
 
 TEMPLATE_DIR = ROOT / "server" / "templates"
 STATIC_FOLDER = ROOT / "server" / "static"
@@ -20,34 +21,37 @@ def index():
 @app.route('/button-click', methods=['POST'])
 def button_click():
     data = request.get_json()
-    sendcommands.ButtonClicked(f"{data['id']}")
+    processcommands.ButtonClicked(f"{data['id']}")
     return '', 204
 @app.route('/button-click-inside', methods=['POST'])
 def button_click_inside():
     data = request.get_json()
-    sendcommands.ButtonClickedInside(f"{data['id']}")
+    processcommands.ButtonClickedInside(f"{data['id']}")
     return '', 204
 @app.route('/button-press', methods=['POST'])
 def button_press():
     data = request.get_json()
-    sendcommands.ButtonPress(f"{data['id']}")
+    processcommands.ButtonPress(f"{data['id']}")
     return '', 204
 @app.route('/button-release', methods=['POST'])
 def button_release():
     data = request.get_json()
-    sendcommands.ButtonRelease(f"{data['id']}")
+    processcommands.ButtonRelease(f"{data['id']}")
     return '', 204
 
 @app.route('/key-down', methods=['POST'])
 def key_down():
     data = request.get_json()
-    sendcommands.ButtonPress(f"{data['key']}")
+    processcommands.ButtonPress(f"{data['key']}")
     return '', 204
 @app.route('/key-up', methods=['POST'])
 def key_up():
     data = request.get_json()
-    sendcommands.ButtonRelease(f"{data['key']}")
+    processcommands.ButtonRelease(f"{data['key']}")
     return '', 204
+@app.route('/video_gesture', endpoint='video_gesture')
+def video_feed():
+    return Response(gesture.gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def start_ui():
     app.run(host='0.0.0.0', port=5000, debug=False)
