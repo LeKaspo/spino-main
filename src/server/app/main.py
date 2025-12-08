caf440323e9b4923e383a51e259c17567f7fadca
sinan.arpert <sinan.arpert@siemens.com>
2025-12-08 10:00:27 +0100

    added speech in main script

--- src/server/app/main.py
+++ src/server/app/main.py
@@ -6,9 +6,11 @@ sys.path.append(str(ROOT))
 
 from server.app.connection import connectionHändler
 import server.app.ui as ui
+import server.speech.speechInput as si
 
 connectionHändler.getInstance()
 #start robo
 ui.start_ui()
+si.main()
 
 # start threads
