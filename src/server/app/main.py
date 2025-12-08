8f03c6a0bd83181506a9025f3ee41facce848cd8
MBSde <martyschn@gmail.com>
2025-12-08 10:51:17 +0100

    created seperate threads for ui and speech

--- src/server/app/main.py
+++ src/server/app/main.py
@@ -1,4 +1,5 @@
 import sys
+import threading
 from pathlib import Path  
 
 ROOT = Path(__file__).resolve().parents[2]
@@ -9,8 +10,14 @@ import server.app.ui as ui
 import server.speech.speechInput as si
 
 connectionHändler.getInstance()
-#start robo
-ui.start_ui()
-si.main()
+# start robo
+ui_thread = threading.Thread(target=ui.start_ui, name="UIThread", daemon=False)
+speech_thread = threading.Thread(target=si.main, name="SpeechThread", daemon=False)
+
+ui_thread.start()
+speech_thread.start()
+
+ui_thread.join()
+speech_thread.join()
 
 # start threads
