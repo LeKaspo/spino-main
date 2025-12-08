342af4b413f1c1c51f46816283c63aeae1f853ad
sinan.arpert <sinan.arpert@siemens.com>
2025-12-08 13:45:04 +0100

    fixed error with sendcommands now only sending commands and not processing them

--- src/server/app/ui.py
+++ src/server/app/ui.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, Response
 ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
-import server.send_commands.sendcommands as sendcommands
+import server.send_commands.processcommands as processcomands
 
 TEMPLATE_DIR = ROOT / "server" / "templates"
 STATIC_FOLDER = ROOT / "server" / "static"
@@ -20,33 +20,33 @@ def index():
 @app.route('/button-click', methods=['POST'])
 def button_click():
     data = request.get_json()
-    sendcommands.ButtonClicked(f"{data['id']}")
+    processcomands.ButtonClicked(f"{data['id']}")
     return '', 204
 @app.route('/button-click-inside', methods=['POST'])
 def button_click_inside():
     data = request.get_json()
-    sendcommands.ButtonClickedInside(f"{data['id']}")
+    processcomands.ButtonClickedInside(f"{data['id']}")
     return '', 204
 @app.route('/button-press', methods=['POST'])
 def button_press():
     data = request.get_json()
-    sendcommands.ButtonPress(f"{data['id']}")
+    processcomands.ButtonPress(f"{data['id']}")
     return '', 204
 @app.route('/button-release', methods=['POST'])
 def button_release():
     data = request.get_json()
-    sendcommands.ButtonRelease(f"{data['id']}")
+    processcomands.ButtonRelease(f"{data['id']}")
     return '', 204
 
 @app.route('/key-down', methods=['POST'])
 def key_down():
     data = request.get_json()
-    sendcommands.ButtonPress(f"{data['key']}")
+    processcomands.ButtonPress(f"{data['key']}")
     return '', 204
 @app.route('/key-up', methods=['POST'])
 def key_up():
     data = request.get_json()
-    sendcommands.ButtonRelease(f"{data['key']}")
+    processcomands.ButtonRelease(f"{data['key']}")
     return '', 204
 
 def start_ui():
