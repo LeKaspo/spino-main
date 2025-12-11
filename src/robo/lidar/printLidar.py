e45f4b12e7a019a5bdb18bfecbd56eecaef70c0b
Aaron Schleihauf <aaron.schleihauf@siemens.com>
2025-12-11 08:06:00 +0100

    print Lidar in thread

--- src/robo/lidar/printLidar.py
+++ src/robo/lidar/printLidar.py
@@ -4,6 +4,12 @@ ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
 from robo.ext_libs.rplidar import RPLidar, RPLidarException
+import threading
+
+def print_lidar():
+    for i, scan in enumerate(lidar.iter_scans()):
+        print(scan)
+        print(len(scan))
 
 lidar2 = RPLidar('/dev/rplidar')
 lidar2.stop()
@@ -18,12 +24,13 @@ print(info)
 health = lidar.get_health()
 print(health)
 
-
-for i, scan in enumerate(lidar.iter_scans()):
-    print(scan)
-    print(len(scan))
-
-
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
+try:
+    t = threading.Thread(target=print_lidar, daemon=True)
+    t.start()
+    t.join()
+except RPLidarException:
+    print("RPLidarException")
+finally:
+    lidar.stop()
+    lidar.stop_motor()
+    lidar.disconnect()
